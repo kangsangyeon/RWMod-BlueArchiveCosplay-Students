a7500f5a7dbb4227160a898d0a0563e004894f5a
sanyoni <ksyline1997@gmail.com>
2023-07-08 05:44:04 +0900

    PawnKindTemplateDef로 Pawn을 스폰하는 기능을 추가합니다.

--- Source/PawnKindTemplateDef.cs
+++ Source/PawnKindTemplateDef.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -11,24 +7,9 @@ namespace BlueArchiveStudents
 {
     public class PawnKindTemplateDef
     {
-        public static bool currentlyGenerating =>
-            operator_Pawn != null;
-
-        protected static Pawn operator_Pawn;
-
         [MustTranslate] public string firstName;
         [MustTranslate] public string lastName;
         [MustTranslate] public string nickname;
-
-        /// <summary>
-        /// 이 pawn의 종족입니다.
-        /// </summary>
-        public ThingDef race;
-
-        /// <summary>
-        /// 이 pawn의 기본 faction(파벌)입니다.
-        /// </summary>
-        public FactionDef defaultFactionType;
 
         public BackstoryDef childHood;
         public BackstoryDef adultHood;
@@ -47,17 +28,6 @@ namespace BlueArchiveStudents
         /// 이 타입의 pawn이 남성인지 또는 여성인지 구분합니다.
         /// </summary>
         public bool isMale = false;
-
-        /// <summary>
-        /// 이 타입의 pawn이 스폰될 때 기본적으로 장착될 무기입니다.
-        /// </summary>
-        public ThingDef weaponDef;
-
-        /// <summary>
-        /// 이 타입의 pawn이 스폰될 때 기본적으로 장착될 의상입니다.
-        /// 목록 중 하나의 의상이 입혀집니다.
-        /// </summary>
-        public List<ThingDef> apparels;
 
         /// <summary>
         /// 이 타입의 pawn의 체형 타입입니다.
@@ -81,5 +51,40 @@ namespace BlueArchiveStudents
 
         public Color skinColor = Color.white;
         public Color hairColor = Color.white;
+
+        /// <summary>
+        /// 이 pawn의 종족입니다.
+        /// </summary>
+        public ThingDef race;
+
+        /// <summary>
+        /// 이 타입의 pawn이 스폰될 때 기본적으로 장착될 무기입니다.
+        /// </summary>
+        public ThingDef weaponDef;
+
+        /// <summary>
+        /// 이 타입의 pawn이 스폰될 때 기본적으로 장착될 의상입니다.
+        /// 목록 중 하나의 의상이 입혀집니다.
+        /// </summary>
+        public List<ThingDef> apparels;
+
+        public void Fill(Pawn _pawn)
+        {
+            _pawn.needs.food.CurLevel = _pawn.needs.food.MaxLevel;
+
+            _pawn.Name = (Name)new NameTriple(this.firstName, this.nickname, this.lastName);
+            _pawn.story.Childhood = this.childHood;
+            _pawn.story.Adulthood = this.age < 20 ? (BackstoryDef)null : this.adultHood;
+            _pawn.ageTracker.AgeBiologicalTicks = (long)this.age * 3600000L;
+            _pawn.ageTracker.AgeChronologicalTicks = (long)this.realAge * 3600000L;
+            _pawn.gender = this.isMale ? Gender.Male : Gender.Female;
+            _pawn.story.bodyType = this.bodyTypeDef;
+            _pawn.story.headType = this.headTypeDef ?? DefDatabase<HeadTypeDef>.GetNamed("Female_NarrowPointy");
+            _pawn.story.hairDef = this.hair == null ? HairDefOf.Bald : this.hair;
+            _pawn.style.beardDef = this.beard == null ? BeardDefOf.NoBeard : this.beard;
+            _pawn.story.skinColorOverride = this.skinColor;
+            _pawn.story.HairColor = this.hairColor;
+            _pawn.Notify_DisabledWorkTypesChanged();
+        }
     }
 }
---
+++ Source/PawnKindTemplateGenerator.cs
@@ -0,0 +1,47 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace BlueArchiveStudents
+{
+    public class PawnKindTemplateGenerator
+    {
+        public static bool currentlyGenerating = false;
+        protected static Pawn generatingPawn;
+
+        public void Generate(PawnKindTemplateDef _pawnKindDef, Map _map)
+        {
+            currentlyGenerating = true;
+            IntVec3 result;
+            if (_map != null && RCellFinder.TryFindRandomPawnEntryCell(out result, _map, 0.2f))
+            {
+                generatingPawn = PawnGenerator.GeneratePawn(PawnKindDefOf.Colonist, Faction.OfPlayer);
+
+                _pawnKindDef.Fill(generatingPawn);
+
+                Thing weapon = null;
+                if (_pawnKindDef.weaponDef != null)
+                    weapon = GenerateAndEquipWeapon(_pawnKindDef, generatingPawn);
+
+                if (ModLister.BiotechInstalled)
+                {
+                    generatingPawn.genes = new Pawn_GeneTracker(generatingPawn);
+                    generatingPawn.genes.SetXenotype(XenotypeDefOf.Baseliner);
+                }
+
+                GenSpawn.Spawn(generatingPawn, result, _map);
+                CameraJumper.TryJump(new GlobalTargetInfo(result, _map));
+            }
+
+            currentlyGenerating = false;
+        }
+
+        private ThingWithComps GenerateAndEquipWeapon(PawnKindTemplateDef _pawnKindDef, Pawn _pawn)
+        {
+            ThingWithComps _weapon = ThingMaker.MakeThing(_pawnKindDef.weaponDef) as ThingWithComps;
+            _weapon.GetComp<CompBiocodable>().CodeFor(_pawn);
+            _pawn.equipment.AddEquipment(_weapon);
+            return _weapon;
+        }
+    }
+}
