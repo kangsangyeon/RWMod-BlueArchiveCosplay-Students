50d446e9117faac2f8eff6be0ca1eb8f26039dd7
sanyoni <ksyline1997@gmail.com>
2024-02-12 01:27:57 +0900

    fix: @UnityProjectScripts에 누락된 파일들을 추가합니다.

---
+++ @UnityProjectScripts/Scene/Accessor/PadAccessor.cs
@@ -0,0 +1,17 @@
+﻿// AUTO GENERATED SOURCE. DO NOT MODIFY.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PadAccessor : MonoBehaviour
+{
+    public Button GachaButton;
+    public Button MissionButton;
+    public Button ShopButton;
+    public Button StudentListButton;
+    public Button AronaButton;
+    public Button SenseiButton;
+    public Button UpdateButton;
+    public Text BlueStoneText;
+    public GameObject AronaPopup;
+}
---
+++ @UnityProjectScripts/Scene/UI/PadUI.cs
@@ -0,0 +1,57 @@
+using System;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+
+namespace @UnityProjectScripts
+{
+    public class PadUI : MonoBehaviour
+    {
+        [SerializeField] private PadAccessor accessor;
+        private Sequence aronaPopupSequence;
+
+        private void Start()
+        {
+            accessor.GachaButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.MissionButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.ShopButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.StudentListButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.AronaButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.SenseiButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+            accessor.UpdateButton.OnClickAsObservable()
+                .Subscribe(_ => ShowAronaPopup())
+                .AddTo(gameObject);
+
+            accessor.AronaPopup.GetComponent<CanvasGroup>().alpha = 0f;
+        }
+
+        private void ShowAronaPopup()
+        {
+            var _canvasGroup = accessor.AronaPopup.GetComponent<CanvasGroup>();
+            var _rectTransform = accessor.AronaPopup.GetComponent<RectTransform>();
+
+            _canvasGroup.alpha = 1f;
+            aronaPopupSequence?.Kill();
+
+            aronaPopupSequence = DOTween.Sequence();
+            aronaPopupSequence
+                .Append(
+                    _rectTransform.DOShakeAnchorPos(
+                        0.3f, Vector3.right * 3f, 15, 0, false, false))
+                .AppendInterval(1)
+                .Append(DOTween.To(() => _canvasGroup.alpha, _v => _canvasGroup.alpha = _v, 0, 0.3f));
+        }
+    }
+}
