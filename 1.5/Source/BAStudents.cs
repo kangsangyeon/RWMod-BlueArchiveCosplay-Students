6df85d0aadee054f01e74a65b1ff51f52f25772b
sanyoni <ksyline1997@gmail.com>
2024-10-02 21:23:51 +0900

    배경 영상 추가, 파티클 재생 끝나면 파괴되도록 수정.

--- 1.5/Source/BAStudents.cs
+++ 1.5/Source/BAStudents.cs
@@ -1,20 +1,20 @@
 using System.Reflection;
+using UnityEngine;
 using Verse;
 
 namespace BA
 {
     [StaticConstructorOnStartup]
-    public static class HarmonyPatcher
-    {
-        static HarmonyPatcher()
-        {
-            new HarmonyLib.Harmony("BlueArchiveStudents")
-                .PatchAll(Assembly.GetExecutingAssembly());
-        }
-    }
-
     public static class BAStudents
     {
         public static bool DisableIMGUI = false;
+
+        static BAStudents()
+        {
+            new HarmonyLib.Harmony("BlueArchiveStudents")
+                .PatchAll(Assembly.GetExecutingAssembly());
+
+            Application.logMessageReceived += (_log, _trace, _type) => { Log.Message($"{_type.ToString()}::{_log}\n{_trace}"); };
+        }
     }
 }
