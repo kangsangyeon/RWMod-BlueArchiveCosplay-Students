9e650489d07369fef9b93dbcadc5c080db3f1091
sanyoni <ksyline1997@gmail.com>
2024-12-04 19:53:06 +0900

    chore: namespace 감싸도록 변경.

--- 1.5/Source/BA/BAUtil.cs
+++ 1.5/Source/BA/BAUtil.cs
@@ -1,21 +1,22 @@
 ﻿using Verse;
 
-namespace BA;
-
-public class BAUtil
+namespace BA
 {
-    public static bool IsBAPawn(Pawn pawn, out Comp_BAPawn comp)
+    public class BAUtil
     {
-        comp = null;
-        if (pawn == null)
-            return false;
-        if (pawn.kindDef.race.race.Humanlike == false) // 사람이 아니면 건너뜀
-            return false;
-        if (pawn.kindDef.race.race.Animal) // 동물이면 건너뜀
-            return false;
-        if (pawn.kindDef.defName.StartsWith("BA") == false) // 우리 프로젝트에서 정의한 pawnkind 아니면 건너뜀
-            return false;
-        comp = pawn.GetComp<Comp_BAPawn>();
-        return true;
+        public static bool IsBAPawn(Pawn pawn, out Comp_BAPawn comp)
+        {
+            comp = null;
+            if (pawn == null)
+                return false;
+            if (pawn.kindDef.race.race.Humanlike == false) // 사람이 아니면 건너뜀
+                return false;
+            if (pawn.kindDef.race.race.Animal) // 동물이면 건너뜀
+                return false;
+            if (pawn.kindDef.defName.StartsWith("BA") == false) // 우리 프로젝트에서 정의한 pawnkind 아니면 건너뜀
+                return false;
+            comp = pawn.GetComp<Comp_BAPawn>();
+            return true;
+        }
     }
 }
