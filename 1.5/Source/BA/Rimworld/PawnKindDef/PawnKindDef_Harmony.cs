3f408a0efbf3e5122578d5c4cb9d778e99de1b78
sanyoni <ksyline1997@gmail.com>
2025-06-05 11:12:42 +0900

    fix: PawnKindDef의 apparelRequired에서 지정한 옷 이외의 옷을 입고 나오는 문제, 지정한 옷을 안입고 나오는 문제 수정.

--- 1.5/Source/BA/Rimworld/PawnKindDef/PawnKindDef_Harmony.cs
+++ 1.5/Source/BA/Rimworld/PawnKindDef/PawnKindDef_Harmony.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using HarmonyLib;
+using RimWorld;
 using Verse;
 
 namespace BA
@@ -47,12 +49,36 @@ namespace BA
             ref Pawn __result,
             PawnGenerationRequest request)
         {
+            var pawn = __result;
+
             if (__result.kindDef is not BA.PawnKindDef kindDef)
                 return;
 
             // biotech가 없을 때, PawnKindDef의 skinColorOverride를 실제 pawn에 강제로 적용함.
             if (!ModsConfig.BiotechActive && kindDef.skinColorOverride.HasValue)
                 __result.story.skinColorOverride = kindDef.skinColorOverride.Value;
+
+            // PawnKindDef에서 apparelRequired로 지정한 옷이 아닌 옷을 입고 나오는 것을 강제로 막음.
+            // (예: faction의 ideology의 선호 의상을 입고 나오기 등.)
+            var notRequiredApparels =
+                pawn.apparel.WornApparel.Where(a =>
+                    !pawn.kindDef.apparelRequired.Exists(t => t == a.def)).ToArray();
+
+            for (int i = 0; i < notRequiredApparels.Length; ++i)
+            {
+                Log.Message($"apparel not required. destroy.: {notRequiredApparels[i].def.defName}.");
+                notRequiredApparels[i].Destroy();
+            }
+
+            // PawnKindDef에서 apparelRequired로 지정한 옷을 입고 나오지 않으면, 옷을 생성해서 강제로 입힘.
+            var requiredButNotWornDefs = pawn.kindDef.apparelRequired.Where(r =>
+                !pawn.apparel.WornApparel.Exists(a => a.def == r)).ToArray();
+
+            for (int i = 0; i < requiredButNotWornDefs.Length; ++i)
+            {
+                Log.Message($"apparel required but not worn. generate.: {requiredButNotWornDefs[i].defName}.");
+                PawnApparelGenerator.GenerateApparelOfDefFor(pawn, requiredButNotWornDefs[i]);
+            }
         }
     }
 }
