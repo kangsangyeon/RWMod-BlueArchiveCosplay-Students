9e1a15e02598b92ecad1ac4988afba00e2da0f59
sanyoni <ksyline1997@gmail.com>
2025-06-09 01:21:55 +0900

    Headgear Lining Layer Added.

--- 1.5/Source/BA/Rimworld/PawnRenderNode/Def/ApparelDef.cs
+++ 1.5/Source/BA/Rimworld/PawnRenderNode/Def/ApparelDef.cs
@@ -5,5 +5,6 @@ namespace BA
     public class ApparelDef : ThingDef
     {
         public string texPathOuterLining; // 외투 안감 텍스쳐
+        public string texPathHeadgearLining; // 모자 안감 텍스쳐
     }
 }
---
+++ 1.5/Source/BA/Rimworld/PawnRenderNode/PawnRenderNode_HeadgearLining.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BA
+{
+    public class PawnRenderNode_HeadgearLining : PawnRenderNode
+    {
+        private readonly Pawn _pawn;
+
+        public PawnRenderNode_HeadgearLining(
+            Pawn pawn,
+            PawnRenderNodeProperties props,
+            PawnRenderTree tree)
+            : base(pawn, props, tree)
+        {
+            _pawn = pawn;
+        }
+        
+        protected override void EnsureMaterialsInitialized()
+        {
+            if (_pawn.DevelopmentalStage.Baby() || _pawn.DevelopmentalStage.Newborn())
+                return;
+            var apparel = _pawn.apparel.WornApparel.FirstOrDefault(x =>
+                x.def.apparel.layers.Contains(ApparelLayerDefOf.Overhead) &&
+                x.def is BA.ApparelDef);
+            if (apparel == null)
+                return;
+            if (apparel.def is not BA.ApparelDef apparelDef)
+                return;
+            if (string.IsNullOrEmpty(apparelDef.texPathHeadgearLining))
+                return;
+            var shader = ShaderDatabase.Cutout;
+            this.graphic = GraphicDatabase.Get<Graphic_Multi>(apparelDef.texPathHeadgearLining, shader, Vector2.one, ColorFor(_pawn));
+        }
+    }
+}
