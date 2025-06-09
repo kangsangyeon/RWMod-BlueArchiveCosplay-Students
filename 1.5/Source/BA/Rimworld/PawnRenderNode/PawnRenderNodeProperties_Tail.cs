c4b6f2d276c96971b83c8f0d52cb013a46ab0e61
sanyoni <ksyline1997@gmail.com>
2025-06-10 00:28:38 +0900

    Add Tail Layer. Add Ibuki Tail.

---
+++ 1.5/Source/BA/Rimworld/PawnRenderNode/PawnRenderNodeProperties_Tail.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace BA
+{
+    public class PawnRenderNodeProperties_Tail : PawnRenderNodeProperties
+    {
+        public bool isNorthOnly;
+    }
+}
---
+++ 1.5/Source/BA/Rimworld/PawnRenderNode/PawnRenderNodeWorker_Tail.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace BA
+{
+    public class PawnRenderNodeWorker_Tail : PawnRenderNodeWorker_FlipWhenCrawling
+    {
+        public override bool CanDrawNow(
+            PawnRenderNode node,
+            PawnDrawParms parms)
+        {
+            bool canDrawNow = base.CanDrawNow(node, parms);
+            if (node.Props is not BA.PawnRenderNodeProperties_Tail tailNodeProps)
+                return canDrawNow;
+            if (tailNodeProps.isNorthOnly)
+            {
+                if (parms.facing != Rot4.North)
+                    return false;
+            }
+            else
+            {
+                if (parms.facing == Rot4.North)
+                    return false;
+            }
+
+            return canDrawNow;
+        }
+    }
+}
